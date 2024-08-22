7dcf3184bbbe48a15c2bfe948f56e220426d1484
Obasi Ngozi <ndukaubangozi@gmail.com>
2024-08-22 08:57:21 +0100

    unittest_in_js

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,13 @@
+const getPaymentTokenFromAPI = require('./6-payment_token');
+const assert = require('assert');
+
+describe('getPaymentTokenFromAPI', function() {
+  it('should return a resolved promise with the object {data: "Successful response from the API"} when success is true', function(done) {
+    getPaymentTokenFromAPI(true)
+      .then((response) => {
+        assert.deepStrictEqual(response, { data: 'Successful response from the API' });
+        done();
+      })
+      .catch((error) => done(error));
+  });
+});
---
+++ 0x06-unittests_in_js/README.md
@@ -0,0 +1,4 @@
+## Unittests in JS
+**`UnitTests`** **`Back-end`** **`JavaScript`** **`ES6`** **`NodeJS`** **`ExpressJS`** **`Mocha`**
+
+![image](https://github.com/iAdamo/alx-backend-javascript/assets/106432903/26a29c0c-f08f-4bd9-9596-f8a22d6a7136)
